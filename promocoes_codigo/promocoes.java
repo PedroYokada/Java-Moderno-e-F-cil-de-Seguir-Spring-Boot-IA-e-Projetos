8641e92858581758a0dd8e25aae4d50a67801ff1
Pedro Yokada <pedro_hlyokada@outlook.com>
2026-07-30 11:11:06 -0300

    mais exemplos de java

---
+++ promocoes_codigo/promocoes.java
@@ -0,0 +1,19 @@
+
+public class promocoes {
+
+    public static void main(String[] args) {
+        byte a = 10;
+        int b = a + 5;
+        System.out.println("Resultado de b: " + b);
+
+        int c = 10;
+        long d = 15L;
+        long e = c + d;
+        System.out.println("Resultado de e: " + e);
+
+        int f = 10;
+        float g = 1.5f;
+        float h = f + g;
+        System.out.println("Resultado de h: " + h);
+    }
+}
