15b8218a2adcb032ed94cf9a965fda08ddc18f76
PedroYokada <pedro_hlyokada@outlook.com>
2026-07-31 20:36:12 -0300

    Exercicio Promoção automatica

---
+++ fundamentos_da_programacao_java/promocao/PromocaoAutomatica.java
@@ -0,0 +1,31 @@
+package promocao;
+
+public class PromocaoAutomatica {
+
+  public static void main(String[] args){
+    byte a = 127;
+    short b = 2089;
+    char letra = 'A';
+    int idade = 10;
+    long habitantes = 100000000L;
+    float metros = 78.90f;
+    double poderLutaGoku = 10000000000000000000000000d;
+    float soma = a + b;
+    float subtracao = a - b;
+    double multiplicacao = metros * poderLutaGoku;
+    double divisao = metros / poderLutaGoku;
+
+    System.out.println("A = " + a);
+    System.out.println("B = " + b);
+    System.out.println("Letra = " + letra);
+    System.out.println("Idade = " + idade);
+    System.out.println("Habitantes = " + habitantes);
+    System.out.println("Metros = " + metros);
+    System.out.println("Poder de luta do Goku = " + poderLutaGoku);
+    System.out.println("A + B = " + soma);
+    System.out.println("A - B = " + subtracao);
+    System.out.println("METROS * PODER DE LUTA DO GOKU = " + multiplicacao);
+    System.out.println("METROS / PODER DE LUTA DO GOKU = " + divisao);
+  }
+  
+}
