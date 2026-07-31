1ee20c7de5ec75938b7b642aebde19a184e5db06
PedroYokada <pedro_hlyokada@outlook.com>
2026-07-31 19:36:30 -0300

    Exercicio Retangulo

---
+++ fundamentos_da_programacao_java/retangulo/CalculadoraAreaRetangulo.java
@@ -0,0 +1,13 @@
+package retangulo;
+
+public class CalculadoraAreaRetangulo {
+  public static void main (String[] args){
+    double largura = 10.90;
+    double altura = 45.78;
+    double  area = altura * largura;
+
+    System.out.println("Largura = " + largura);
+    System.out.println("Altura = " + altura);
+    System.out.println("Área = " + area);
+  }
+}
