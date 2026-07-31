2433563d848dcc83987b608a83e64f69bbf4813d
PedroYokada <pedro_hlyokada@outlook.com>
2026-07-31 19:29:31 -0300

    Exercicio de controle de estoque de padaria

---
+++ fundamentos_da_programacao_java/ControleEstoquePadaria/ControleEstoquePadaria.java
@@ -0,0 +1,11 @@
+package ControleEstoquePadaria;
+
+public class ControleEstoquePadaria {
+  public static void main(String[] args){
+    int quantidadeFarinha = 10;
+    int quantidadeAcucar = 20;
+
+    System.out.println("Quantidade de açucar = " + quantidadeAcucar);
+    System.out.println("Quantidade de farinha = " + quantidadeFarinha);
+  }
+}
---
+++ fundamentos_da_programacao_java/comentarios/comentarios.java
@@ -0,0 +1,8 @@
+package comentarios;
+
+public class comentarios {
+  public static void main(String[] args) {
+    int farinha = 2;
+    System.out.println("A receita requer " + farinha + " xícaras de farinha.");
+  }
+}
