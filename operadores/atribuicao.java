6355ff9123cdd5fc6ed83e6b78ff6df93e5bdc47
PedroYokada <pedro_hlyokada@outlook.com>
2026-08-06 16:09:14 -0300

    Operadores de comparação exercicio

---
+++ operadores/atribuicao.java
@@ -0,0 +1,15 @@
+package operadores;
+
+public class atribuicao {
+  public static void main(String[] args) {
+    double a = 1000.10;
+    double b = 1000.20;
+
+    System.out.println(a + " = " + b + " ,a condição é: " + (a == b));
+    System.out.println(a + " != " + b + " ,a condição é: " + (a != b));
+    System.out.println(a + " > " + b + " ,a condição é: " + (a > b));
+    System.out.println(a + " < " + b + " ,a condição é: " + (a < b));
+    System.out.println(a + " >= " + b + " ,a condição é: " + (a >= b));
+    System.out.println(a + " <= " + b + " ,a condição é: " + (a <= b));
+  }
+}
