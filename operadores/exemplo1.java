9c4d4ae5b810af21d78e264ea7377106f637c8cd
PedroYokada <pedro_hlyokada@outlook.com>
2026-08-06 15:49:34 -0300

    Operadores aritméticos exercicio

--- operadores/exemplo1.java
+++ operadores/exemplo1.java
@@ -1,5 +1,19 @@
 package operadores;
-
 public class exemplo1 {
-  
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 2;
+
+    System.out.println(a + " + " + b + " = " + (a + b));
+    /*
+     * Aqui foi necessário envolver a subtração (a - b) em parênteses,
+     * pois o operador '+' e o '-' têm a mesma precedência.
+     * Sem os parênteses, o Java tenta subtrair um número de uma String,
+     * o que causa um erro de compilação no código.
+     */
+    System.out.println(a + " - " + b + " = " + (a - b));
+    System.out.println(a + " * " + b + " = " + (a * b));
+    System.out.println(a + " / " + b + " = " + (a / b));
+    System.out.println(a + " % " + b + " = " + (a % b));
+  }
 }
