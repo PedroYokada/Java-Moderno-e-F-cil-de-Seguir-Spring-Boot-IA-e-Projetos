527136174bd36753dedca8b93487607af63d788e
Pedro Yokada <pedro_hlyokada@outlook.com>
2026-08-12 09:58:35 -0300

    Exercicios de incremento e decremento

---
+++ exercicios/IncrementoDecremento.java
@@ -0,0 +1,17 @@
+
+public class IncrementoDecremento {
+
+    public static void main(String[] args) {
+        int contador = 10;
+
+        System.out.println("CONTADOR ANTES DO INCREMENTO = " + contador);
+        contador++;
+        System.out.println("CONTADOR DEPOIS DO INCREMENTO = " + contador);
+
+        
+        System.out.println("CONTADOR ANTES DO DECREMENTO = " + contador);
+        contador--;
+        System.out.println("CONTADOR DEPOIS DO DECREMENTO = " + contador);
+
+    }
+}
